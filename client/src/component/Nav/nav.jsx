444c3413c982e2b7106063f55be516ccf4b992e9
franco <91661089+davidfranncoo@users.noreply.github.com>
2024-02-20 00:05:40 -0300

    a

--- client/src/component/Nav/nav.jsx
+++ client/src/component/Nav/nav.jsx
@@ -1,53 +1,119 @@
 import React, { useState } from "react";
+import instagramIcon from "../../icons/instagram.svg";
+import linkedinIcon from "../../icons/linkedin.svg";
+import githubIcon from "../../icons/github.svg";
+import closeIcon from "../../icons/close.svg";
+import openIcon from "../../icons/open.svg";
 import "./nav.scss";
 
-
-
-export default function Nav({scrollToHome,scrollToContact,scrollToTools,scrollToProyects,scrollToAboutme}) {
+export default function Nav({
+  scrollToHome,
+  scrollToContact,
+  scrollToTools,
+  scrollToProyects,
+  scrollToAboutme,
+}) {
   const [showMenu, setShowMenu] = useState(false);
 
-  function openClose(){
+  function openClose() {
     setShowMenu(!showMenu);
-  }; 
+  }
 
   return (
     <div>
-      {/* <div className={`menu1 ${showMenu ? 'show_menu2' : ''}`}>
-      <button id="open" style={{ display: showMenu ? 'none' : 'block' }} onClick={openClose}>
-        Open
-      </button>
-      <button id="close" style={{ display: showMenu ? 'block' : 'none' }} onClick={openClose}>
-        Close
-      </button>
-      <div className="h1_menu">
-
-      <h1 >ESTOYYYYYY</h1>
-      </div>
-    </div> */}
       <nav className="Brand">
-        <ul className="menu" id="menu">
+        <div className="menu" id="menu">
           
-          <button  onClick={scrollToHome}className="btn-43">
-            <span  className="old">Inicio</span>
-            <span className="new">Inicio</span>
-          </button>
-          <button onClick={scrollToAboutme} className="btn-43">
-            <span className="old">Sobre mi</span>
-            <span className="new">Sobre mi</span>
-          </button>
-          <button onClick={scrollToTools} className="btn-43">
-            <span className="old">Herramientas</span>
-            <span className="new">Herramientas</span>
-          </button>{" "}
-          <button onClick={scrollToProyects} className="btn-43">
-            <span className="old">Proyectos</span>
-            <span className="new">Proyectos</span>
-          </button>
-          <button onClick={scrollToContact} className="btn-43">
-            <span className="old">Contacto</span>
-            <span className="new">Contacto</span>
-          </button>
-        </ul>
+          <div className="imgs_icons">
+           <a 
+                href="https://www.instagram.com/davidfranncoo/"
+                target="_blank"
+                rel="noopener noreferrer">
+
+            <img className="img_icono " src={instagramIcon}></img>
+           </a>
+           <a 
+                href="https://www.linkedin.com/in/franco-segovia-23a397236/"
+                target="_blank"
+                rel="noopener noreferrer">
+
+            <img className="img_icono" src={linkedinIcon}></img>
+                </a>
+                <a 
+                href="https://github.com/davidfranncoo"
+                target="_blank"
+                rel="noopener noreferrer">
+
+            <img className="img_icono" src={githubIcon}></img>
+                </a>
+          </div>
+
+          <div className={`menu1 ${showMenu ? "show_menu2" : ""}`}>
+            <button
+              className=" button_open"
+              id="open"
+              style={{ display: showMenu ? "none" : "block" }}
+              onClick={openClose}
+            >
+              <img className="img_icono" src={openIcon}></img>
+            </button>
+            <div className="h1_menu">
+              <button
+                className=" button_close"
+                id="close"
+                style={{ display: showMenu ? "block" : "none" }}
+                onClick={openClose}
+              >
+                <img className="img_icono" src={closeIcon}></img>
+              </button>
+              <div className="menu_buttoms_openclose">
+            <button onClick={scrollToHome} className="btn-43">
+              <span className="old">Inicio</span>
+              <span className="new">Inicio</span>
+            </button>
+            <button onClick={scrollToAboutme} className="btn-43">
+              <span className="old">Sobre mi</span>
+              <span className="new">Sobre mi</span>
+            </button>
+            <button onClick={scrollToTools} className="btn-43">
+              <span className="old">Herramientas</span>
+              <span className="new">Herramientas</span>
+            </button>{" "}
+            <button onClick={scrollToProyects} className="btn-43">
+              <span className="old">Proyectos</span>
+              <span className="new">Proyectos</span>
+            </button>
+            <button onClick={scrollToContact} className="btn-43">
+              <span className="old">Contacto</span>
+              <span className="new">Contacto</span>
+            </button>
+          </div>
+            </div>
+          </div>
+
+          <div className="menu_buttoms">
+            <button onClick={scrollToHome} className="btn-43">
+              <span className="old">Inicio</span>
+              <span className="new">Inicio</span>
+            </button>
+            <button onClick={scrollToAboutme} className="btn-43">
+              <span className="old">Sobre mi</span>
+              <span className="new">Sobre mi</span>
+            </button>
+            <button onClick={scrollToTools} className="btn-43">
+              <span className="old">Herramientas</span>
+              <span className="new">Herramientas</span>
+            </button>{" "}
+            <button onClick={scrollToProyects} className="btn-43">
+              <span className="old">Proyectos</span>
+              <span className="new">Proyectos</span>
+            </button>
+            <button onClick={scrollToContact} className="btn-43">
+              <span className="old">Contacto</span>
+              <span className="new">Contacto</span>
+            </button>
+          </div>
+        </div>
       </nav>
     </div>
   );
